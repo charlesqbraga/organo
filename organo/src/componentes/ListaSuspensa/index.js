e1c3d93961e2626698ec23fe5d1f4217368b92b3
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-17 16:27:16 -0300

    Update index.js

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -7,7 +7,7 @@ const ListaSuspensa = (props) => {
         <div className="lista-suspensa"> 
             <label>{props.label}</label>
             <select>
-                {props.itens.map(item => {<option key={item}>{item}</option>})}
+                {props.itens.map(item => {return <option key={item}> {item} </option>})}
             </select>
         </div>
     )
