56158b5261e4d32c57d74bf538fd37fc524f0e4d
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-17 16:14:36 -0300

    implementação da lista suspensa

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,14 +1,22 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 const Formulario = () => {
+
+    const times = [
+        'Gato',
+        'Cachorro', 
+        'Outros'
+    ]
     return (
-        <section className="formulario">
+        <section className="formulario" >
             <form>
                 <h2>Preencha os dados do pet que você deseja doar</h2>
-                <CampoTexto label="Tipo" placeholder="O seu pet é um cão, um gato, um páss..." />
+                <ListaSuspensa label="Tipo" itens={times}/>
                 <CampoTexto label="Nome" placeholder="Digite nome do pet" />
                 <CampoTexto label="Raça" placeholder="Digite a raça do pet" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                
             </form>
         </section>
     )
